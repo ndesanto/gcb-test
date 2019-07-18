09ac5844c27591b13ec2d21ed3ff0208da4b1618
Nick <nick@minutebox.com>
2019-07-18 10:50:43 -0400

    test) check gcb

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-self.response.write('Automatically!')
+self.response.write('Automatically2!')
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
